2a93f6f3b54b47bf7d7fa3afcae164923618ea47
AlexAlexGame <listru2005@mail.ru>
2016-08-15 13:26:41 +0300

    Initial Commit

---
+++ MVP_test_M/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVP_test_M
+//
+//  Created by alex zolin on 15.08.16.
+//  Copyright © 2016 alex zolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
